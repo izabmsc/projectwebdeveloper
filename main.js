0de241544534394e2ea50b19c50eba53658c0edf
Izabella Marques <izabmsc@gmail.com>
2024-01-12 11:21:08 -0300

    adicionando prompt

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let firstName = 'Izabella'
-let lastName ='Marques'
-let age = 28
+let firstName = prompt('Digite seu primeiro nome: ')
+let lastName = prompt('Qual o seu sobrenome: ')
+let age = prompt('Qual a sua idade? ')
+
 
 console.log('meu nome é ' + firstName + ' '+ lastName + ' '+ 'tenho ' + age)
