ccdde1367cee04d38e8525da0467e8f397164252
Izabella Marques <izabmsc@gmail.com>
2024-01-12 11:45:29 -0300

    incluindo o css na estilização

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let firstName = prompt('Digite seu primeiro nome: ')
-let lastName = prompt('Qual o seu sobrenome: ')
-let age = parseInt (prompt('Qual a sua idade? '))
-age = age + 2
-const age2 = 2
-age = age + age2
+//let firstName = prompt('Digite seu primeiro nome: ')
+//let lastName = prompt('Qual o seu sobrenome: ')
+//let age = parseInt (prompt('Qual a sua idade? '))
+//age = age + 2
+//const age2 = 2
+//age = age + age2
 
 
-console.log('meu nome é ' + firstName + ' '+ lastName + ' '+ 'tenho ' + age)
-console.log(age)
-console.log(age +age2)
+//console.log('meu nome é ' + firstName + ' '+ lastName + ' '+ 'tenho ' + age)
+//console.log(age)
+//console.log(age +age2)
