44cbf44d32c31d53876635d027ed70d89260a87b
Izabella Marques <izabmsc@gmail.com>
2024-02-11 13:59:18 -0300

    if else e if else >=

--- main.js
+++ main.js
@@ -32,8 +32,43 @@
 //console.log (resultado)
 
 //Celsius em F
-let celsius = 0
-let Fahrenheit = 0
-celsius = parseFloat (prompt ("Digitite a temperatura em celsius"))//float é o controle de casas decimais
-Fahrenheit = celsius * 1.8 + 32
-alert ('A temperatura em Fahrenheit é de ' + Fahrenheit.toFixed(2))
+//let celsius = 0
+//let Fahrenheit = 0
+//celsius = parseFloat (prompt ("Digitite a temperatura em celsius")) //float é o controle de casas decimais
+//Fahrenheit = celsius * 1.8 + 32
+//alert ('A temperatura em Fahrenheit é de ' + Fahrenheit.toFixed(2))
+
+//IDADE IF E ELSE
+//let age = prompt('Qual a sua idade?')
+//if (age >=18)
+//{console.log ('você é um adulto')}
+//else if (age >=15){
+//    console.log ('você é um adolescente')}
+//else {
+//    console.log ('você não um adulto')}
+
+
+//nota do aluno if else
+//if else pode ser utilizado para sempre que estiver alguma mudança no resultado pretendido, e encerrar com else que no caso seria nenhuma das opções listadas
+let grade = prompt ('digite a nota do aluno:')
+if (grade > 100)
+alert ('nota invalida')
+else if (grade >= 90)
+{
+    alert ('Aluno nota A')
+}
+else if (grade >= 80)
+{
+    alert ('Aluno nota B')
+}
+else if (grade >= 70)
+{
+    alert ('Aluno nota C')
+}
+else if (grade >= 60)
+{
+    alert ('Aluno nota D')
+}
+else {
+    alert ('Aluno nota F')
+}
