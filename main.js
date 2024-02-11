0057dd0572a9762b36a25950e2f5a4b749c3a85a
Izabella Marques <izabmsc@gmail.com>
2024-02-11 11:08:33 -0300

    anna

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let firstName = prompt('Digite seu primeiro nome: ')
-document.getElementById ("greeting").innerHTML = 'Hello' +' '+ firstName
-let greeting = 'Hello'
+//let firstName = prompt('Digite seu primeiro nome: ')
+//document.getElementById ("greeting").innerHTML = 'Hello' +' '+ firstName
+//let greeting = 'Hello'
 //let lastName = prompt('Qual o seu sobrenome: ')
 //let age = parseInt (prompt('Qual a sua idade? '))
 //age = age + 2
@@ -11,3 +11,10 @@ let greeting = 'Hello'
 //console.log('meu nome é ' + firstName + ' '+ lastName + ' '+ 'tenho ' + age)
 //console.log(age)
 //console.log(age +age2)
+
+let friends =['Alan', 'Mark', 'Julia', 'Bella', 4]
+console.log (friends[1])
+friends[1] = 'Carlinhos'
+console.log (friends[1])
+friends[5] = 'Anna'
+console.log (friends)
