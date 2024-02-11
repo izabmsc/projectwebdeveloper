c8c985bd8499eac46d46aecd1ce1f38f11be3665
Izabella Marques <izabmsc@gmail.com>
2024-02-11 12:14:07 -0300

    celsius em F

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //let firstName = prompt('Digite seu primeiro nome: ')
 //document.getElementById ("greeting").innerHTML = 'Hello' +' '+ firstName
 //let greeting = 'Hello'
-//let lastName = prompt('Qual o seu sobrenome: ')
+//let lastName = prompt('Qual o seu sobrenome: ') //prompt vai pedir para colocar informações na tela para ser adicionada
 //let age = parseInt (prompt('Qual a sua idade? '))
 //age = age + 2
 //const age2 = 2
@@ -12,13 +12,28 @@
 //console.log(age)
 //console.log(age +age2)
 
-let friends =['Alan', 'Mark', 'Julia', 'Bella']
-console.log (friends)
+//let friends =['Alan', 'Mark', 'Julia', 'Bella']
+//console.log (friends)
 
-friends [1] = 'Anna'
-friends[1] = 'Carlinhos'
-friends[5] = 1
+//friends [1] = 'Anna'
+//friends[1] = 'Carlinhos'
+//friends[5] = 1
 
-console.log (friends)//array com as alterações "friends"
-console.log (friends[1])//array com 1 na lista
+//console.log (friends)//array com as alterações "friends"
+//console.log (friends[1])//array com 1 na lista
 //sempre que tiver a variavel, qualquer alteração tem que imprimir de novo
+
+//operações matematicas - para forçar a logica da matemetica colocar entre parenteses o que sera feito primeiro
+//let a = 10 //int.
+//let b = 5 //int.
+//let = 2.5 //float
+
+//let resultado = a + b
+//console.log (resultado)
+
+//Celsius em F
+let celsius = 0
+let Fahrenheit = 0
+celsius = parseFloat (prompt ("Digitite a temperatura em celsius"))//float é o controle de casas decimais
+Fahrenheit = celsius * 1.8 + 32
+alert ('A temperatura em Fahrenheit é de ' + Fahrenheit.toFixed(2))
